32dfcbd4ceab6042aa8b9d159982192f3600b924
KatarinaMandic <katarina.mandic.97@gmail.com>
2022-11-03 13:49:00 +0100

    Modified calculator - layout

--- calculator.py
+++ calculator.py
@@ -2,11 +2,17 @@ import PySimpleGUI as sg
 
 sg.theme('DarkBlue14')
 layout = [
-    [sg.Text('x: '), sg.Input(key='_X_'), sg.Text('y: '), sg.Input(key = '_Y_')],
-    [sg.Button('+'), sg.Button('-'), sg.Button('*'), sg.Button('/')]
+    [sg.Text('x: '), sg.Input(key='_X_', size = (6,1)), 
+    sg.Text('y: '), sg.Input(key = '_Y_', size = (6,1))],
+    [sg.Button('+', size = (6,1)), 
+    sg.Button('-', size = (6,1)), 
+    sg.Button('*', size = (6,1)), 
+    sg.Button('/', size = (6,1))],
+    [sg.Text('', size = (40,1), key = '_RES_')]
 ]
 
-window = sg.Window('Calculator', layout=layout)
+window = sg.Window('Calculator', layout=layout, 
+size = (400,200), font = ('Arial', 16))
 
 while True:
     event, values = window.read()
@@ -20,14 +26,14 @@ while True:
         sg.popup('One of the values is not a number.')
     else: 
         if event == '+':
-            sg.popup(f'{x} + {y} = {x+y}')
+            window['_RES_'].Update(f'{x} + {y} = {x+y}')
         if event == '-':
-            sg.popup(f'{x} - {y} = {x-y}')
+            window['_RES_'].Update(f'{x} - {y} = {x-y}')
         if event == '*':
-            sg.popup(f'{x} * {y} = {x*y}')
+            window['_RES_'].Update(f'{x} * {y} = {x*y}')
         if event == '/':
             try:
-                sg.popup(f'{x} / {y} = {x/y}')
+                window['_RES_'].Update(f'{x} / {y} = {x/y}')
             except ZeroDivisionError:
                 sg.popup('Zero Division is not allowed.')
 
